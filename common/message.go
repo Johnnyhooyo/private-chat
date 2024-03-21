a399d1ef2fa57f809e46ea443d9669f662dcaa8c
Enzo <huyuanqiang@papegames.net>
2024-03-21 15:29:57 +0800

    增加文件传输

--- common/message.go
+++ common/message.go
@@ -3,6 +3,7 @@ package common
 import (
 	"fmt"
 	"github.com/johnnhooyo/private-chat/common/route"
+	"os"
 )
 
 type UserInfo struct {
@@ -11,15 +12,28 @@ type UserInfo struct {
 }
 
 type Message struct {
-	Route      route.Type `json:"route,omitempty"`
-	From       *UserInfo  `json:"from,omitempty"`
-	To         *UserInfo  `json:"to,omitempty"`
-	Body       any        `json:"body,omitempty"` // 如果有图片之类的 用${attach_1} 格式填充，然后去attachFile中按index读取
-	AttachFile [][]byte   `json:"attach_file,omitempty"`
+	Route      route.Type        `json:"route,omitempty"`
+	From       *UserInfo         `json:"from,omitempty"`
+	To         *UserInfo         `json:"to,omitempty"`
+	Body       any               `json:"body,omitempty"` // 如果有图片之类的 用[pic:(.*?)] 格式填充，然后去attachFile中按index读取
+	AttachFile map[string][]byte `json:"attach_file,omitempty"`
 }
 
 func (m *Message) ReceiveMsg() {
 	fmt.Printf("from:%s:%s \n", m.From.Name, m.Body)
+	for fileName, bytes := range m.AttachFile {
+		if _, err := os.Stat(fmt.Sprintf("./%s/", m.From.Name)); os.IsNotExist(err) {
+			err = os.MkdirAll(fmt.Sprintf("./%s/", m.From.Name), 0755)
+			if err != nil {
+				fmt.Println("无法创建目标文件夹：", err)
+				return
+			}
+		}
+		err := os.WriteFile(fmt.Sprintf("./%s/", m.From.Name)+fileName, bytes, 0644)
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+	}
 }
 
 func (m *Message) SendMsg() {
